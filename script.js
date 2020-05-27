4a77e6d19f7fd283f6ce582d889183fdbf8303f7
Hello Lala <laura.abowd@gmail.com>
2020-05-27 12:26:53 -0500

    added js toggle function for nav-links

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+document.getElementById('menu-btn').onclick = function() {
+  toggleShowMenus();
+}
 
+document.getElementById('order-btn').onclick = function() {
+  toggleShowOrder();
+}
 
-function toggle() {
-  const menuLink = document.getElementbyId('menu').classList.toggle("show");
+function toggleShowMenus() {
+ document.getElementById('menu').classList.toggle("show");
+};
+
+function toggleShowOrder() {
+  document.getElementById('online-ordering').classList.toggle("show");
 }
 
 window.onclick = function(event) {
   if (!event.target.matches('.btn__nav')) {
-    const dropdowns = document.getElementbyClassName("menu-dropdown-content");
+    const dropdowns = document.getElementsByClassName("dropdown-content");
     let i;
     for (i = 0; i < dropdowns.length; i++) {
+
       let openDropdown = dropdowns[i];
       if(openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
