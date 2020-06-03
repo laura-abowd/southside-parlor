f77dfd4be726d51ba1540d0e2d9bc31881681d2b
Hello Lala <laura.abowd@gmail.com>
2020-06-03 17:06:35 -0500

    removed focus on button and added view menu links

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-document.getElementById('menu-btn').onclick = function() {
-  toggleShowMenus();
+
+// <--- show menu pdfs --->
+const element = document.querySelectorAll(".menu-display");
+for (let x = 0; x < element.length; x++)
+  element[x].style.display = 'none';
+
+const menu_elements = document.querySelectorAll(".a-menu__items");
+const items = document.querySelectorAll(".menu-display");
+
+for(let i = 0; i < menu_elements.length; i++) {
+  menu_elements[i].addEventListener("click", function() {
+    menu_elements.forEach(function(li) {
+      li.classList.remove("active");
+    });
+    this.classList.add("active");
+    let li_value = this.getAttribute("data-notes");
+    items.forEach(function(item) {
+      item.style.display ="none";
+    })
+  })
 }
-
-document.getElementById('order-btn').onclick = function() {
-  toggleShowOrder();
-}
-
-function toggleShowMenus() {
- document.getElementById('menu').classList.toggle("show");
-};
-
-function toggleShowOrder() {
-  document.getElementById('online-ordering').classList.toggle("show");
-}
-
-window.addEventListener('click', function(event) {
-  if (!event.target.matches('.btn__nav')) {
-    const dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-
-      let openDropdown = dropdowns[i];
-      if(openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-});
-
-
